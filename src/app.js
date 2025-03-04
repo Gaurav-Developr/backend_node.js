595812caee498fbf34c4df49d9b5acc34b4d9b5b
Gaurav Malik <gauravmalik5233@gmail.com>
2025-03-04 16:38:25 +0530

    utils code files

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials : true
+}));
+// *******  app.use(.use)  ************ hmesa confirgation ya middleware k liye use me aata hai 
+// app(express) uses json and make limit otherwise you got more and more json file so one time your server will crash so i am use limit
+app.use(express.json({limit:"16kb"}));
+// url s data lene k liye urlencoded ka use karte hai jo bhi data url s aayea(upr wale url s)(with use confirgation)
+app.use(express.urlencoded({extended:true,limit:"16kb"}));
+//pdf/image file ko store karne k liye apne server me he on over public folder(with use confirgation)
+app.use(express.static("public"));
+//isse hm apne server(ya server s he) s, user k browser s cookie le b skte hai aur usme crud operation b perform karskte hai
+app.use(express.cookieParser);
+
+
+
+
+
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -7,8 +7,17 @@ dotenv.config({
     path:"./env"
 })
 
-
 db()
+
+.then(()=> {
+    app.listen(process.env.Port ||8000, () =>{
+        console.log(`Server is running on port ${process.env.Port}`);
+    } )
+})
+.catch((error) => {
+    console.log("Database connection error on index.js file !!",error)
+}
+)
 
 
 // ;( async () => {
