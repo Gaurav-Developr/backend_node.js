81898be6e8fe0bf0a18c4c13222818b983b07923
Gaurav Malik <gauravmalik5233@gmail.com>
2025-03-05 12:20:45 +0530

    middleware(hooks) and create JWT tokens and refreshtokens

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -23,11 +23,11 @@ const userSchema = new Schema({
         index:true
     },
     avatar:{
-        type:String,   //cloudinary url(third party m rakhte hai bas vha URL aajata hai)
+        type:String,   //cloudinary url(third party m rakhte hai bas vha s URL aajata hai)
         required:true,
     },
     coverImage:{
-        type:String, //cloudinary url(third party m rakhte hai bas vha URL aajata hai)
+        type:String, //cloudinary url(third party m rakhte hai bas vha s URL aajata hai)
     },
     watchHistory:[
         {
@@ -44,5 +44,42 @@ const userSchema = new Schema({
     }
 },{timestamps:true})
 
+// hooks pre ko hooks bolte hai yeh middleware he hote aur middleware me hm pre (this."yeh hooks hote hai") hooks ka use karrhe hai.(data save hone s phle yeh hooks chle)
+// password incript before save on my database
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password"))return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+// compare password (check user password and incript password is same)
+userSchema.methods.isPasswordCorrect= async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+// apne methods inject karne 
+// Generate ** JWT ** Token (for create jwt token to sharing data)
+userSchema.methods.generateAccessToken = async function(){
+return Jwt.sign({
+    _id:this._id,
+    username:this.username,
+    fullname:this.fullname,
+    email:this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+// Generate ** JWT ** Refresh Token 
+userSchema.methods.generateRefreshToken = async function(){
+    jwt.sign({
+        _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 
 export const User = mongoose.model("User",userSchema)
